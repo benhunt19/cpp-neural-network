5c56ba5d8e093fcdadd43964b9789039b5927552
Ben Hunt <ben.hunt19@hotmail.com>
2024-12-16 23:35:05 +0000

    Add project files.

---
+++ cpp-neural-network.cpp
@@ -0,0 +1,11 @@
+// cpp-neural-network.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include <iostream>
+#include "header.h"
+
+using namespace std;
+
+int main()
+{
+    testFunc();
+}
