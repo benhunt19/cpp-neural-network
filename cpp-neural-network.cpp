1e34cb21afda2a0a19f274710aa453982ee7fedb
Ben Hunt <ben.hunt19@hotmail.com>
2024-12-21 18:43:20 +0000

    working files and number zipper

--- cpp-neural-network.cpp
+++ cpp-neural-network.cpp
@@ -12,13 +12,30 @@ using namespace std;
 const string trainImagesPath = "C:/Users/benhu/PycharmProjects/Data Sets/mnist-dataset/versions/1/train-images-idx3-ubyte";
 const string trainImagesFile = "/train-images-idx3-ubyte";
 // Labels
-const string trainLabelsPath = "C:/Users/benhu/PycharmProjects/Data Sets/mnist-dataset/versions/1/train-labels-idx3-ubyte";
-const string trainLabelsFile = "/train-labels-idx3-ubyte";
+const string trainLabelsPath = "C:/Users/benhu/PycharmProjects/Data Sets/mnist-dataset/versions/1/train-labels-idx1-ubyte";
+const string trainLabelsFile = "/train-labels-idx1-ubyte";
 
 int main()
 {
-	File imagesTest = File(trainImagesPath, trainImagesFile);
-	imagesTest.printImageAtIndex(210);
-	imagesTest.getImage(10);
+	File trainingData = File(trainImagesPath, trainImagesFile);
+	File labelData = File(trainLabelsPath, trainLabelsFile);
+
+	int start_index = 0;
+	int end_index = 5000;
+
+	vector<Image> training_images = trainingData.getImageVector(start_index, end_index);
+	vector<char> training_labels = labelData.getLabelVector(start_index, end_index);
+
+	// pair images and labels;
+	for (int i = start_index; i < end_index; i++) {
+		training_images[i].setLabel(training_labels[i]);
+	}
+	
+	for (int i = 0; i < 10; i++) {
+		cout << int(training_images[i].label) << endl;
+		training_images[i].print();
+	}
+
+
 	return 1;
 }
--- globalClasses.h
+++ globalClasses.h
@@ -6,16 +6,60 @@
 using namespace std;
 
 
-class File {
+class Image {
 public:
 	int32_t num_rows = 0;
 	int32_t num_cols = 0;
-	int32_t num_images = 0;
-	int32_t magic_number = 0;
-	int32_t heaederOffset = 16;
+	int32_t image_size = 0;
+	vector<uint8_t> image_data = {};
+	char label;
+
+	Image(vector<uint8_t> image_data_in, int32_t num_rows_in, int32_t num_cols_in, int32_t image_size_in) {
+		image_data = image_data_in;
+		image_size = image_size_in;
+		num_rows = num_rows_in;
+		num_cols = num_cols_in;
+	};
+
+	void print() {
+		char tmp = 0;
+		for (int x = 0; x < image_data.size(); x++) {
+			tmp = image_data[x] == 0 ? ' ' : '*';
+			cout << tmp;
+			if ((x + 1) % num_cols == 0) cout << endl;
+		};
+		cout << endl;
+	};
+
+	void setLabel(char label_in) {
+		label = label_in;
+	};
+};
+
+
+class File {
+public:
 	string filePath;
 	string fName;
+	string type = ""; // 'images' or  'labels'
+	int32_t magic_number = 0;
+	// Image Data
+	int32_t num_rows = 0;
+	int32_t num_cols = 0;
+	int32_t num_images = 0;
+	int32_t image_offset = 16;
 	int32_t image_size = 0;
+	// Label data
+	int32_t num_labels = 0;
+	int32_t label_offset = 8;
+
+	// processing function
+	int32_t swapEndian(int32_t value) {
+		return ((value & 0xFF000000) >> 24) |
+			((value & 0x00FF0000) >> 8) |
+			((value & 0x0000FF00) << 8) |
+			((value & 0x000000FF) << 24);
+	};
 
 
 	File(string path, string filename) {
@@ -33,90 +77,117 @@ public:
 			magic_number = swapEndian(magic_number); // Swap byte order manually
 			cout << "Magic Number: " << magic_number << endl;
 
-			// Second 4 bytes are the number of images
-			file.read(reinterpret_cast<char*>(&num_images), sizeof(num_images));
-			num_images = swapEndian(num_images);
-			cout << "Number of Images: " << num_images << endl;
+			if (magic_number == 2051) {
+				type = "images";
+				// Second 4 bytes are the number of images
+				file.read(reinterpret_cast<char*>(&num_images), sizeof(num_images));
+				num_images = swapEndian(num_images);
+				cout << "Number of Images: " << num_images << endl;
 
-			// Third 4 bytes are the number of rows
-			file.read(reinterpret_cast<char*>(&num_rows), sizeof(num_rows));
-			num_rows = swapEndian(num_rows);
-			cout << "Number of Rows: " << num_rows << endl;
+				// Third 4 bytes are the number of rows
+				file.read(reinterpret_cast<char*>(&num_rows), sizeof(num_rows));
+				num_rows = swapEndian(num_rows);
+				cout << "Number of Rows: " << num_rows << endl;
 
-			// Fourth 4 bytes are the number of columns
-			file.read(reinterpret_cast<char*>(&num_cols), sizeof(num_cols));
-			num_cols = swapEndian(num_cols);
-			cout << "Number of Columns: " << num_cols << endl;
+				// Fourth 4 bytes are the number of columns
+				file.read(reinterpret_cast<char*>(&num_cols), sizeof(num_cols));
+				num_cols = swapEndian(num_cols);
+				cout << "Number of Columns: " << num_cols << endl;
 
-			// Calculate image size
-			image_size = num_rows * num_cols;
-			cout << "innit image_size" << image_size << endl;
-
+				// Calculate image size
+				image_size = num_rows * num_cols;
+				cout << "innit image_size: " << image_size << endl;
+			}
+			else if (magic_number == 2049)  {
+				type = "labels";
+				file.read((char*)&num_labels, sizeof(num_labels));
+				num_labels = swapEndian(num_labels);
+				cout << "Num labels: " << num_labels << endl;
+			}
 			file.close();
 		}
 		
 	}
 
-	// processing function
-	int32_t swapEndian(int32_t value) {
-		return ((value & 0xFF000000) >> 24) | // Move byte 4 to byte 1
-			((value & 0x00FF0000) >> 8) | // Move byte 3 to byte 2
-			((value & 0x0000FF00) << 8) | // Move byte 2 to byte 3
-			((value & 0x000000FF) << 24);  // Move byte 1 to byte 4
+	Image getImage(int image_index) {
+		if (type != "images") throw runtime_error("File Type Not Images");
+		vector<uint8_t> image_data(image_size);
+		ifstream file((filePath).c_str(), ios::binary);
+		file.open((filePath + fName).c_str(), ios::binary);
+		int offset = image_offset + image_index * image_size; // Header + image offset
+		file.seekg(offset, std::ios::beg); // Move to the specific image
+		file.read(reinterpret_cast<char*>(image_data.data()), image_size);
+		file.close();
+		return Image(image_data, num_rows, num_cols, image_size); // review this;
 	};
+
 
 	void printImageAtIndex(int image_index) {
-		vector<uint8_t> image_data(image_size);
-		ifstream file((filePath).c_str(), ios::binary);
-		file.open((filePath + fName).c_str(), ios::binary);
-		int offset = 16 + image_index * image_size; // Header + image offset
-		file.seekg(offset, std::ios::beg); // Move to the specific image
-		file.read(reinterpret_cast<char*>(image_data.data()), image_size);
-		float tmp = 0;
-		for (int x = 0; x < image_data.size(); x++) {
-			tmp = image_data[x] == 0 ? 0 : 1;
-			cout << tmp;
-			if ((x + 1) % num_cols == 0) cout << endl;
-		};
-		cout << endl;
+		if (type != "images") throw runtime_error("File Type Not Images");
+		Image imageToPrint = getImage(image_index);
+		imageToPrint.print();
 	};
 
-	void getImage(int image_index) {
+	vector<Image> getImageVector(int start_index, int end_index) {
+		if (type != "images") throw runtime_error("File Type Not Images");
+		vector<Image> return_vec = {};
 		vector<uint8_t> image_data(image_size);
 		ifstream file((filePath).c_str(), ios::binary);
+		int offset;
+
 		file.open((filePath + fName).c_str(), ios::binary);
-		int offset = 16 + image_index * image_size; // Header + image offset
-		file.seekg(offset, std::ios::beg); // Move to the specific image
-		file.read(reinterpret_cast<char*>(image_data.data()), image_size);
-		//Image retImage;// = Image(image_data, num_rows, num_cols, 784); // review this
+		for (int index = start_index; index < end_index; index++) {
+			offset = image_offset + (index * image_size); // Header + image offset
+			file.seekg(offset, std::ios::beg);
+			file.read(reinterpret_cast<char*>(image_data.data()), image_size);
+			return_vec.push_back(Image(image_data, num_rows, num_cols, image_size));
+		}
+		file.close();
+
+		return return_vec;
+
+	};
+
+	char getLabelAtIndex(int label_index) {
+		char label;
+		ifstream file((filePath).c_str(), ios::binary);
+		file.open((filePath + fName).c_str(), ios::binary);
+		int offset = label_offset + label_index; // Header + label offset
+		file.seekg(offset, ios::beg); // Move to the label
+		file.read(&label, 1);
+		file.close();
+		cout << int(label) << endl;
+		return label;
+	};
+
+	vector<char> getLabelVector(int start_index, int end_index) {
+		if (type != "labels") throw runtime_error("File Type Not Images");
+		vector<char> return_vec;
+		ifstream file((filePath).c_str(), ios::binary);
+		int offset;
+		char label;
+		file.open((filePath + fName).c_str(), ios::binary);
+		for (int index = start_index; index < end_index; index++) {
+			offset = label_offset + (index * 1); // Label offset and index, (chars are 1 byte) 
+			file.seekg(offset, ios::beg);
+			file.read(&label, 1);
+			return_vec.push_back(label);
+		}
+		file.close();
+		return return_vec;
 	}
 
+};
 
+class NetworkLayer {
+public:
 
 };
 
 
-class Image {
+
+// potentially move this to it's own file
+class NeuralNetwork {
 public:
-	int32_t num_rows = 0;
-	int32_t num_cols = 0;
-	int32_t image_size = 0;
-	vector<uint8_t> image_data = {};
 
-	Image(vector<uint8_t> image_data_in, int32_t num_rows_in, int32_t num_cols_in, int32_t image_size_in) {
-		image_data = image_data_in;
-		image_size = image_size_in;
-		num_rows = num_rows_in;
-		num_cols = num_cols_in;
-	};
-
-	void print() {
-		int32_t tmp = 0;
-		for (int x = 0; x < image_data.size(); x++) {
-			tmp = image_data[x] == 0 ? 0 : 1;
-			cout << tmp;
-			if ((x + 1) % image_size == 0) cout << endl;
-		};
-		cout << endl;
-	};
 };
