1dc02f86a238bf6fec4c681256c87d284b5f3346
Ben Hunt <111769195+benhunt19@users.noreply.github.com>
2025-01-13 15:12:24 +0000

    Bracket Refactor (#6)

--- cpp-neural-network.cpp
+++ cpp-neural-network.cpp
@@ -20,8 +20,8 @@ const string trainLabelsFile = "/train-labels-idx1-ubyte";
 const string testLabelsPath = "C:/Users/benhu/PycharmProjects/Data Sets/mnist-dataset/versions/1/t10k-labels-idx1-ubyte";
 const string testLabelsFile = "/t10k-labels-idx1-ubyte";
 
-int main()
-{
+int main() {
+	// Get training data
     File trainingImageData = File(trainImagesPath, trainImagesFile);
     File trainingLabelData = File(trainLabelsPath, trainLabelsFile);
 
@@ -36,8 +36,7 @@ int main()
     int final_layer_count = 10; // labels are 0, 1, 2, ... , 9 
 
     // zip together the training images and their corresponding labels
-    for (int i = start_index; i < end_index; i++)
-    {
+    for (int i = start_index; i < end_index; i++) {
         training_images[i].setLabel(training_labels[i]);
     }
 
@@ -71,8 +70,7 @@ int main()
     vector<char> test_labels = testLabelData.getLabelVector(start_index_test, end_index_test);
 
     // zip together the test images and their corresponding labels
-    for (int i = start_index_test; i < end_index_test; i++)
-    {
+    for (int i = start_index_test; i < end_index_test; i++) {
         test_images[i].setLabel(test_labels[i]);
     }
 
